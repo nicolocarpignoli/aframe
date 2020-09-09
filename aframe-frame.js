8e5590ad11007472852490799bec46d2da923442
Jonas Johansson <jns.johansson@gmail.com>
2020-09-09 03:45:34 +0200

    Update aframe-frame.js

--- aframe-frame.js
+++ aframe-frame.js
@@ -1,10 +1,12 @@
 AFRAME.registerComponent('frame', {
 	schema: {
-		size: { default: 3 }
+		size: { default: 3 },
+		scale: { default: 30 }
 	},
 	init: function () {
 		const self = this
-		const SIZE = this.data.size
+		const SIZE = this.data.size * 0.5
+		const SCALE = this.data.scale
 		const HALF_PI = Math.PI / 2
 
 		self.el.id = 'frame'
@@ -15,21 +17,21 @@ AFRAME.registerComponent('frame', {
 			plane.object3D.rotation.set(-HALF_PI, 0, 0)
 			plane.setAttribute('occlude', true)
 			switch (pos) {
-				case 'top':
-					plane.object3D.position.set(-SIZE, 0, 0)
-					plane.object3D.scale.set(SIZE, SIZE, 1)
-					break
-				case 'bottom':
-					plane.object3D.position.set(SIZE, 0, 0)
-					plane.object3D.scale.set(SIZE, SIZE, 1)
+				case 'right':
+					plane.object3D.position.set(SIZE + SCALE / 2, 0, 0)
+					plane.object3D.scale.set(SCALE, SCALE, 1)
 					break
 				case 'left':
-					plane.object3D.position.set(0, 0, -SIZE)
-					plane.object3D.scale.set(SIZE * 3, SIZE, 1)
+					plane.object3D.position.set(-(SIZE + SCALE / 2), 0, 0)
+					plane.object3D.scale.set(SCALE, SCALE, 1)
 					break
-				case 'right':
-					plane.object3D.position.set(0, 0, SIZE)
-					plane.object3D.scale.set(SIZE * 3, SIZE, 1)
+				case 'top':
+					plane.object3D.position.set(0, 0, -(SIZE + SCALE / 2))
+					plane.object3D.scale.set(SCALE * 3, SCALE, 1)
+					break
+				case 'bottom':
+					plane.object3D.position.set(0, 0, SIZE + SCALE / 2)
+					plane.object3D.scale.set(SCALE * 3, SCALE, 1)
 					break
 			}
 			self.update()
