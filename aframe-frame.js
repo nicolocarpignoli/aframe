ddc979e204fc5ea252db61d981c4fef058ad4ccd
Jonas Johansson <jns.johansson@gmail.com>
2020-09-09 03:34:43 +0200

    Update aframe-frame.js

--- aframe-frame.js
+++ aframe-frame.js
@@ -1,12 +1,10 @@
 AFRAME.registerComponent('frame', {
 	schema: {
-		size: { default: 3 },
-		scale: { default: 20 }
+		size: { default: 3 }
 	},
 	init: function () {
 		const self = this
 		const SIZE = this.data.size
-		const SCALE = this.data.scale
 		const HALF_PI = Math.PI / 2
 
 		self.el.id = 'frame'
@@ -19,19 +17,19 @@ AFRAME.registerComponent('frame', {
 			switch (pos) {
 				case 'top':
 					plane.object3D.position.set(-SIZE, 0, 0)
-					plane.object3D.scale.set(SCALE, SCALE, 1)
+					plane.object3D.scale.set(SIZE, SIZE, 1)
 					break
 				case 'bottom':
 					plane.object3D.position.set(SIZE, 0, 0)
-					plane.object3D.scale.set(SCALE, SCALE, 1)
+					plane.object3D.scale.set(SIZE, SIZE, 1)
 					break
 				case 'left':
 					plane.object3D.position.set(0, 0, -SIZE)
-					plane.object3D.scale.set(SCALE * 3, SCALE, 1)
+					plane.object3D.scale.set(SIZE * 3, SIZE, 1)
 					break
 				case 'right':
 					plane.object3D.position.set(0, 0, SIZE)
-					plane.object3D.scale.set(SCALE * 3, SCALE, 1)
+					plane.object3D.scale.set(SIZE * 3, SIZE, 1)
 					break
 			}
 			self.update()
