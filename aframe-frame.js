d886d21ca60576d5a9382e8d2237b9a00e0f5159
Jonas Johansson <jns.johansson@gmail.com>
2020-09-09 04:04:17 +0200

    Update aframe-frame.js

--- aframe-frame.js
+++ aframe-frame.js
@@ -1,37 +1,48 @@
 AFRAME.registerComponent('frame', {
 	schema: {
-		size: { default: 3 },
-		scale: { default: 100 }
+		size: { default: 2 },
+		scale: { default: 40 }
 	},
 	init: function () {
 		const self = this
-		const SIZE = this.data.size * 0.5
+		const SIZE = this.data.size * 0.25
 		const SCALE = this.data.scale
 		const HALF_PI = Math.PI / 2
 
 		self.el.id = 'frame'
+		self.el.object3D.position.set(0, 0.1, 0)
+		self.el.object3D.scale.set(1, 1, 1)
 
 		for (let pos of ['top', 'bottom', 'left', 'right']) {
 			let plane = document.createElement('a-plane')
 			plane.id = pos
-			plane.object3D.rotation.set(-HALF_PI, 0, 0)
+			plane.setAttribute('rotation', '-90 0 0')
+			//plane.object3D.rotation.set(-HALF_PI, 0, 0);
 			plane.setAttribute('occlude', true)
 			switch (pos) {
 				case 'right':
-					plane.object3D.position.set(SIZE + SCALE / 2, 0, 0)
-					plane.object3D.scale.set(SCALE, SCALE, 1)
+					plane.setAttribute('position', `${SIZE + SCALE / 2} 0 0`)
+					plane.setAttribute('scale', `${SCALE} ${SCALE} 1`)
+					//plane.object3D.position.set(SIZE + SCALE / 2, 0, 0);
+					//plane.object3D.scale.set(SCALE, SCALE, 1);
 					break
 				case 'left':
-					plane.object3D.position.set(-(SIZE + SCALE / 2), 0, 0)
-					plane.object3D.scale.set(SCALE, SCALE, 1)
+					plane.setAttribute('position', `${-(SIZE + SCALE / 2)} 0 0`)
+					plane.setAttribute('scale', `${SCALE} ${SCALE} 1`)
+					//plane.object3D.position.set(-(SIZE + SCALE / 2), 0, 0);
+					//plane.object3D.scale.set(SCALE, SCALE, 1);
 					break
 				case 'top':
-					plane.object3D.position.set(0, 0, -(SIZE + SCALE / 2))
-					plane.object3D.scale.set(SCALE * 3, SCALE, 1)
+					plane.setAttribute('position', `0, 0, ${-(SIZE + SCALE / 2)}`)
+					plane.setAttribute('scale', `${SCALE * 3} ${SCALE} 1`)
+					//plane.object3D.position.set(0, 0, -(SIZE + SCALE / 2));
+					//plane.object3D.scale.set(SCALE * 3, SCALE, 1);
 					break
 				case 'bottom':
-					plane.object3D.position.set(0, 0, SIZE + SCALE / 2)
-					plane.object3D.scale.set(SCALE * 3, SCALE, 1)
+					plane.setAttribute('position', `0, 0, ${SIZE + SCALE / 2}`)
+					plane.setAttribute('scale', `${SCALE * 3} ${SCALE} 1`)
+					//plane.object3D.position.set(0, 0, SIZE + SCALE / 2);
+					//plane.object3D.scale.set(SCALE * 3, SCALE, 1);
 					break
 			}
 			self.update()
