1c10eb921c0fe6130ddacd3c825cf6875bd98f3b
Jonas Johansson <jns.johansson@gmail.com>
2020-10-03 13:46:14 +0300

    Update aframe-material-update.js

--- aframe-material-update.js
+++ aframe-material-update.js
@@ -1,54 +1,57 @@
 AFRAME.registerComponent('material-update', {
-	schema: {
-		target:	{default: null},
-		src: {default: null},
-		ambient: {default: null, type: 'color'},
-		color: {default: null, type: 'color'},
-		emissive: {default: null, type: 'color'},
-		shininess: {type: 'number'},
-		specular: {default: null, type: 'color'},
-		repeat: {default: ''},
-	},
-	init() {
-		const data = this.data;
-		this.el.addEventListener('model-loaded', e => {
-			const object = e.detail.model;
-			object.traverse(node => {
-				if (node.material !== undefined){
-					// if (node.material.map.encoding !== null){
-					// 	node.material.map.encoding = 3000; // https://github.com/mrdoob/three.js/issues/6593, https://github.com/mrdoob/three.js/issues/14419
-					// } // https://github.com/aframevr/aframe/issues/3263
+  schema: {
+    target: { default: null },
+    src: { default: null },
+    ambient: { default: null, type: 'color' },
+    // color: { default: null, type: 'color' },
+    color: { default: null, type: 'string' },
+    emissive: { default: null, type: 'color' },
+    shininess: { type: 'number' },
+    specular: { default: null, type: 'color' },
+    repeat: { default: '' },
+    new: { default: false, type: 'boolean' }
+  },
+  init() {
+    const data = this.data
+    this.el.addEventListener('model-loaded', e => {
+      const object = e.detail.model
+      object.traverse(node => {
+        if (node.material !== undefined) {
+          // if (node.material.map.encoding !== null){
+          // 	node.material.map.encoding = 3000; // https://github.com/mrdoob/three.js/issues/6593, https://github.com/mrdoob/three.js/issues/14419
+          // } // https://github.com/aframevr/aframe/issues/3263
+        } else {
+          node.material = new THREE.StandardMaterial({})
+        }
+        if (node.name === data.target) {
+          if (data.src !== null) {
+            const texPath = document.querySelector(data.src).getAttribute('src')
+            const tex = new THREE.TextureLoader().load(texPath)
+            node.material.map = tex
+          }
 
-				}
-				if (node.name === data.target) {
+          // if (data.new) node.material = new THREE.StandardMaterial({})
 
-					if (data.src !== null) {
-						const texPath = document.querySelector(data.src).getAttribute('src');
-						const tex = new THREE.TextureLoader().load(texPath);
-						node.material.map = tex;
-					}
+          if (data.ambient !== null)
+            node.material.ambient = new THREE.Color(data.ambient)
 
-					if (data.ambient !== null)
-						node.material.ambient = new THREE.Color(data.ambient);
+          if (data.color !== null)
+            node.material.color = new THREE.Color(data.color)
 
-					if (data.color !== null)
-						node.material.color = new THREE.Color(data.color);
+          if (data.emissive !== null)
+            node.material.emissive = new THREE.Color(data.emissive)
 
-					if (data.emissive !== null)
-						node.material.emissive = new THREE.Color(data.emissive);
+          if (data.shininess !== null) node.material.shininess = data.shininess
 
-					if (data.shininess !== null)
-						node.material.shininess = data.shininess;
+          if (data.specular !== null)
+            node.material.specular = new THREE.Color(data.specular)
 
-					if (data.specular !== null)
-						node.material.specular = new THREE.Color(data.specular);
+          if (data.repeat !== '')
+            node.material.map.repeat.set(data.repeat, data.repeat)
 
-					if (data.repeat !== '')
-						node.material.map.repeat.set( data.repeat, data.repeat );
-
-					node.material.needsUpdate = true;
-				}
-			});
-		});
-	}
-});
+          node.material.needsUpdate = true
+        }
+      })
+    })
+  }
+})
