42942e40beebb85466e70f5051bc939a62cd38a7
Jonas Johansson <jns.johansson@gmail.com>
2020-10-09 00:16:31 +0200

    Update aframe-frame.js

--- aframe-frame.js
+++ aframe-frame.js
@@ -1,44 +1,84 @@
 AFRAME.registerComponent('frame', {
   schema: {
-    entity: { default: 'a-plane' },
-    size: { default: 2 },
-    scale: { default: 100 }
+    width: { default: 1 },
+    height: { default: 1 },
+    scale: { default: 10 },
+    borderSize: { default: 0 }
   },
   init: function() {
-    this.marker = this.el.parentNode
-    const SIZE = this.data.size * 0.25
+    const WIDTH = this.data.width
+    const HEIGHT = this.data.height
+    const DEPTH = 0.1
+    const HALF_WIDTH = WIDTH / 2
+    const HALF_HEIGHT = HEIGHT / 2
+    const HALF_DEPTH = DEPTH / 2
     const SCALE = this.data.scale
+    const HALF_SCALE = SCALE / 2
     const HALF_PI = Math.PI / 2
+    const BORDER_SIZE = this.data.borderSize
+    const HALF_BORDER = BORDER_SIZE / 2
 
-    this.el.id = 'frame'
-    this.el.object3D.position.set(0, 0.1, 0)
-    this.el.object3D.scale.set(1, 1, 1)
+    const PARTS = ['right', 'left', 'top', 'bottom']
 
-    for (let pos of ['top', 'bottom', 'left', 'right']) {
-      let part = document.createElement(this.data.entity)
-      part.id = pos
-      part.object3D.rotation.set(-HALF_PI, 0, 0)
-      part.setAttribute('occlude', true)
-      switch (pos) {
-        case 'right':
-          part.object3D.position.set(SIZE + SCALE / 2, 0, 0)
-          part.object3D.scale.set(SCALE, SCALE, 1)
-          break
-        case 'left':
-          part.object3D.position.set(-(SIZE + SCALE / 2), 0, 0)
-          part.object3D.scale.set(SCALE, SCALE, 1)
-          break
-        case 'top':
-          part.object3D.position.set(0, 0, -(SIZE + SCALE / 2))
-          part.object3D.scale.set(SCALE * 3, SCALE, 1)
-          break
-        case 'bottom':
-          part.object3D.position.set(0, 0, SIZE + SCALE / 2)
-          part.object3D.scale.set(SCALE * 3, SCALE, 1)
-          break
-      }
-      this.update()
-      this.el.appendChild(part)
+    const self = this.el
+
+    self.id = 'frame'
+    self.object3D.position.set(0, 0, 0)
+    self.object3D.scale.set(1, 1, 1)
+
+    addFrameOccluder()
+    if (BORDER_SIZE > 0) addFrame()
+
+    this.update()
+
+    function addFrameOccluder() {
+      const parts = ['left', 'right', 'top', 'bottom'].map(name => {
+        const el = document.createElement('a-plane')
+        el.id = name
+        el.setAttribute('occlude', true)
+        el.object3D.rotation.set(HALF_PI, 0, 0)
+        el.object3D.scale.set(SCALE, SCALE, 0)
+        self.appendChild(el)
+        return el
+      })
+
+      parts[0].object3D.position.set(HALF_WIDTH + HALF_SCALE, 0, 0)
+      parts[1].object3D.position.set(-(HALF_WIDTH + HALF_SCALE), 0, 0)
+      parts[2].object3D.position.set(0, 0, -(HALF_HEIGHT + HALF_SCALE))
+      parts[3].object3D.position.set(0, 0, HALF_HEIGHT + HALF_SCALE)
     }
+
+    function addFrame() {
+      const parts = ['left', 'right', 'top', 'bottom'].map(name => {
+        const el = document.createElement('a-box')
+        el.setAttribute('color', 'black')
+        el.id = name
+        self.appendChild(el)
+        return el
+      })
+
+      parts[0].object3D.position.set(HALF_WIDTH - HALF_BORDER, -HALF_DEPTH, 0)
+      parts[0].object3D.scale.set(BORDER_SIZE, DEPTH, HEIGHT)
+
+      parts[1].object3D.position.set(-(HALF_WIDTH - HALF_BORDER), -HALF_DEPTH, 0)
+      parts[1].object3D.scale.set(BORDER_SIZE, DEPTH, HEIGHT)
+
+      parts[2].object3D.position.set(0, -HALF_DEPTH, -(HALF_HEIGHT - HALF_BORDER))
+      parts[2].object3D.scale.set(WIDTH, DEPTH, BORDER_SIZE)
+
+      parts[3].object3D.position.set(0, -HALF_DEPTH, HALF_HEIGHT - HALF_BORDER)
+      parts[3].object3D.scale.set(WIDTH, DEPTH, BORDER_SIZE)
+    }
+  }
+})
+
+AFRAME.registerComponent('occlude', {
+  init: function() {
+    var el = this.el
+    var mesh = el.getObject3D('mesh')
+    var material = new THREE.MeshBasicMaterial({
+      colorWrite: false
+    })
+    mesh.material = material
   }
 })
