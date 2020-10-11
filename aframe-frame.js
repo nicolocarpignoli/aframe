88116788a541455786fa2b2fbd3e13988ed5eee6
Jonas Johansson <jns.johansson@gmail.com>
2020-10-11 13:56:51 +0200

    update

--- aframe-frame.js
+++ aframe-frame.js
@@ -3,7 +3,7 @@ AFRAME.registerComponent('frame', {
     width: { default: 1 },
     height: { default: 1 },
     scale: { default: 20 },
-    frameEl: { default: '' }
+    frameEl: { default: '', type: "selector" }
   },
   init: function() {
     const WIDTH = this.data.width
@@ -13,15 +13,18 @@ AFRAME.registerComponent('frame', {
     const SCALE = this.data.scale
     const HALF_SCALE = SCALE / 2
     const HALF_PI = Math.PI / 2
-    const FRAME_EL = document.querySelector(this.data.frameEl)
+    const FRAME_EL = this.data.frameEl
 
     const self = this.el
     self.id = 'frame'
+
+    console.log(FRAME_EL)
 
     addFrameOccluder()
     if (FRAME_EL) addFrame()
 
     function addFrameOccluder() {
+      console.log("Adding frameOccluder")
       const parts = ['left', 'right', 'top', 'bottom'].map(name => {
         const el = document.createElement('a-plane')
         el.id = name
@@ -39,6 +42,7 @@ AFRAME.registerComponent('frame', {
     }
 
     function addFrame() {
+      console.log("Adding frame")
       const BORDER_SIZE = FRAME_EL.object3D.scale.x
       const DEPTH = FRAME_EL.object3D.scale.z
       const HALF_DEPTH = DEPTH / 2
@@ -48,22 +52,26 @@ AFRAME.registerComponent('frame', {
 
       const parts = ['left', 'right', 'top', 'bottom'].map(name => {
         const el = FRAME_EL.cloneNode(true)
+        el.setAttribute('visible', true)
         el.id = name
         self.appendChild(el)
         return el
       })
 
-      parts[0].object3D.position.set(-(HALF_WIDTH - HALF_BORDER), -HALF_DEPTH, 0)
-      parts[0].setAttribute('scale', `${BORDER_SIZE}, ${DEPTH}, ${HEIGHT - 2 * BORDER_SIZE}`)
+      parts[0].object3D.position.set(-(HALF_WIDTH + HALF_BORDER), HALF_DEPTH, 0)
+      parts[0].setAttribute('scale', `${BORDER_SIZE}, ${DEPTH}, ${HEIGHT + 2 * BORDER_SIZE}`)
 
-      parts[1].object3D.position.set(HALF_WIDTH - HALF_BORDER, -HALF_DEPTH, 0)
-      parts[1].setAttribute('scale', `${BORDER_SIZE}, ${DEPTH}, ${HEIGHT - 2 * BORDER_SIZE}`)
+      parts[1].object3D.position.set(HALF_WIDTH + HALF_BORDER, HALF_DEPTH, 0)
+      parts[1].setAttribute('scale', `${BORDER_SIZE}, ${DEPTH}, ${HEIGHT + 2 * BORDER_SIZE}`)
 
-      parts[2].object3D.position.set(0, -HALF_DEPTH, -(HALF_HEIGHT - HALF_BORDER))
-      parts[2].setAttribute('scale', `${WIDTH}, ${DEPTH}, ${BORDER_SIZE}`)
+      parts[2].object3D.position.set(0, HALF_DEPTH, -(HALF_HEIGHT + HALF_BORDER))
+      parts[2].object3D.rotation.set(0, HALF_PI, 0)
+      parts[2].setAttribute('scale', `${BORDER_SIZE}, ${DEPTH}, ${HEIGHT}`)
+      // setAttribute('scale', `${WIDTH}, ${DEPTH}, ${BORDER_SIZE}`)
 
-      parts[3].object3D.position.set(0, -HALF_DEPTH, HALF_HEIGHT - HALF_BORDER)
-      parts[3].setAttribute('scale', `${WIDTH}, ${DEPTH}, ${BORDER_SIZE}`)
+      parts[3].object3D.position.set(0, HALF_DEPTH, HALF_HEIGHT + HALF_BORDER)
+      parts[3].object3D.rotation.set(0, HALF_PI, 0)
+      parts[3].setAttribute('scale', `${BORDER_SIZE}, ${DEPTH}, ${HEIGHT}`)
     }
 
     this.update()
